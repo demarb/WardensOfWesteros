d0758ca9d2004ac2177d2391082c166d334b7572
demarb <demarcbrown01@gmail.com>
2023-07-23 20:31:42 -0400

    deploy setup

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/WardensOfWesteros">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
