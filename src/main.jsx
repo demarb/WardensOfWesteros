384e53434ed8bfef53731b7b310ee3c07490f6e2
demarb <demarcbrown01@gmail.com>
2023-07-23 21:27:58 -0400

    Switch to has =h router and build

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/WardensOfWesteros">
-      <App />
+    <BrowserRouter>
+
+      <HashRouter basename="/">
+        <App />
+      </HashRouter>
+      
     </BrowserRouter>
   </React.StrictMode>,
 )
