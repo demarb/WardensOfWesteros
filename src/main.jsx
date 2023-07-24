bec6398bb69757d9c9057c8e981a6579e6bf667f
demarb <demarcbrown01@gmail.com>
2023-07-23 21:41:34 -0400

    Change basename

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
 
-      <HashRouter basename="/">
+      <HashRouter basename="/WardensOfWesteros/">
         <App />
       </HashRouter>
       
