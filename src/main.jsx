64e3061634bc8699cf7be15a7f99ed200d8d446d
demarb <demarcbrown01@gmail.com>
2023-07-23 21:59:54 -0400

    Removed deploy changes

--- src/main.jsx
+++ src/main.jsx
@@ -9,9 +9,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
 
-      <HashRouter basename="/WardensOfWesteros/">
+      {/* <HashRouter basename="/WardensOfWesteros/"> */}
         <App />
-      </HashRouter>
+      {/* </HashRouter> */}
       
     </BrowserRouter>
   </React.StrictMode>,
